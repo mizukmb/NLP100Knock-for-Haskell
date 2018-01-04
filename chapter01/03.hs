621a3b995be64978836a9248dc583061664845e4
mizukmb <mizukmb6@gmail.com>
2018-01-05 00:13:21 +0900

    :recycle: Refactor

--- chapter01/03.hs
+++ chapter01/03.hs
@@ -1,7 +1,7 @@
 import           Data.List.Split
 
 ratioFromWords :: [Char] -> [Int]
-ratioFromWords str = map length (splitOn " " str)
+ratioFromWords = map length . splitOn " "
 
 -- *Main > ratioFromWords "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 -- [3,1,4,1,6,9,2,7,5,3,5,8,9,7,10]
